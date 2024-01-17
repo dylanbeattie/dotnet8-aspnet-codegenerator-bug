4b1264fb6a8a804aca083b41bc6e6eb997c183d9
Dylan Beattie <dylan@dylanbeattie.net>
2024-01-17 19:01:44 +0000

    Isolated reproduction case for problem using aspnet-codegenerator 8.x with extension methods

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # dotnet8-aspnet-codegenerator-bug
-Reproduction code for a bug with Microsoft.VisualStudio.Web.CodeGeneration.Design 8.0.0 when calling extension methods from Program.cs
+Reproduction code for a bug in `Microsoft.VisualStudio.Web.CodeGeneration.Design` 8.0.0
+
+#### Symptoms:
+
+The bug occurs when trying to run `dotnet aspnet-codegenerator` and specify a `DbContext`:
+
+```
+dotnet aspnet-codegenerator controller -name ArtistsController -m Artist -dc MyDbContext
+```
+
+If the application's `Program.Main` method calls any extension methods, the tool fails with:
+
+```
+Could not get the reflection type for DbContext : MyDbContext
+  at Microsoft.VisualStudio.Web.CodeGeneration.ActionInvoker.<BuildCommandLine>b__6_0()
+  at Microsoft.Extensions.CommandLineUtils.CommandLineApplication.Execute(String[] args)
+  at Microsoft.VisualStudio.Web.CodeGeneration.ActionInvoker.Execute(String[] args)
+  at Microsoft.VisualStudio.Web.CodeGeneration.CodeGenCommand.Execute(String[] args)
+```
+
+This happens with projects that reference `Microsoft.VisualStudio.Web.CodeGeneration.Design` v8.0.0. The same code works fine with `Microsoft.VisualStudio.Web.CodeGeneration.Design` v7.0.1, running under both .NET 7 and under .NET 8.
+
+Two example projects are provided:
+
+[dotnet7](dotnet7) references EF Core 7.x and Microsoft.VisualStudio.Web.CodeGeneration.Design 7.0.1
+
+[dotnet8](dotnet8) references EF Core 8.x and Microsoft.VisualStudio.Web.CodeGeneration.Design 8.0.0
+
+To reproduce the problem, check out the repo, open the `dotnet8` project:
+
+```
+dotnet build
+dotnet aspnet-codegenerator controller -name ArtistsController -m Artist -dc MyDbContext
+```
+
+To verify the cause, comment out  `Program.cs` line 18:
+
+```csharp
+// builder.DoNothing();
+```
+
+and re-run the `aspnet-codegenerator` command.
+
+
+
---
+++ dotnet7/Program.cs
@@ -0,0 +1,60 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+/* The codegen command that reproduces the problem:
+
+ dotnet aspnet-codegenerator controller -name ArtistsController -m Artist -dc MyDbContext
+
+If you uncomment the call to builder.DoNothing(), this command fails with:
+  Could not get the reflection type for DbContext : MyDbContext
+    at Microsoft.VisualStudio.Web.CodeGeneration.ActionInvoker.<BuildCommandLine>b__6_0()
+    at Microsoft.Extensions.CommandLineUtils.CommandLineApplication.Execute(String[] args)
+    at Microsoft.VisualStudio.Web.CodeGeneration.ActionInvoker.Execute(String[] args)
+    at Microsoft.VisualStudio.Web.CodeGeneration.CodeGenCommand.Execute(String[] args)
+*/
+
+builder.DoNothing();
+
+builder.Services.AddControllersWithViews();
+var sqliteConnection = new SqliteConnection("Data Source=:memory:");
+sqliteConnection.Open();
+builder.Services.AddDbContext<MyDbContext>(options => options.UseSqlite(sqliteConnection));
+var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope()) {
+	var db = scope.ServiceProvider.GetService<MyDbContext>()!;
+	db.Database.EnsureCreated();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapGet("/", (MyDbContext db) => db.Artists.ToList());
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+app.Run();
+
+public static class ExtensionMethods {
+	public static bool DoNothing(this WebApplicationBuilder builder) => true;
+}
+
+public class Artist {
+	public Guid Id { get; set; }
+	public string Name { get; set; } = String.Empty;
+}
+
+public class MyDbContext : DbContext {
+	public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+	public DbSet<Artist> Artists { get; set; } = default!;
+	protected override void OnModelCreating(ModelBuilder modelBuilder) 	{
+		base.OnModelCreating(modelBuilder);
+		modelBuilder.Entity<Artist>().HasData(
+			new Artist { Id = Guid.NewGuid(), Name = "Artist A" },
+			new Artist { Id = Guid.NewGuid(), Name = "Artist B" },
+			new Artist { Id = Guid.NewGuid(), Name = "Artist C" }
+		);
+	}
+}
+
